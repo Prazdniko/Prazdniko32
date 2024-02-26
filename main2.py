02ca72e360309eeeb81559306b4547e7e9816816
Prazdniko <161280244+Prazdniko@users.noreply.github.com>
2024-02-26 17:20:49 +0300

    main2.py

--- main2.py
+++ main2.py
@@ -8,5 +8,10 @@ with open('space.csv', encoding='utf-8') as data_file:
     csv_data = reader(data_file, delimiter=',')
     # Линейным поиском получить ответ
     for row in csv_data:
+        if "ShipName" in row[6] > 5:
+            x = row[6] + row[1]
+        if "ShipName" in row[7] > 3:
+            y = row[7] + len("planet")  + row[3]
+            print(f'Координаты по x: {x}, координаты по y - {row[2]}')
         if "LOCA" in row[1]:
             print(f'Координаты по x: {row[4]}, координаты по y - {row[2]}')
