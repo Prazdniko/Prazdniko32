1dfa1da3e6eab990f94a95514b2953a38fa7229e
student <89266861864@mail.ru>
2024-02-26 17:03:09 +0300

    commit massage

--- main.py
+++ main.py
@@ -3,8 +3,10 @@
 from csv import reader
 
 group_data = 0
-with open('space.txt', encoding='utf-8') as data_file:
-    class ShipName:
+with open('space.csv', encoding='utf-8') as data_file:
+    # Открыть файл с данными как объект reader
+    csv_data = reader(data_file, delimiter=',')
+    # Линейным поиском получить ответ:
     group_data = input('Введи название карабля или в введите STOP: ')
     while group_data != 'STOP':
         group_data = input('Введи название карабля или в введите STOP: ')
